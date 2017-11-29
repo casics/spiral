c7bc18b47cf021b0676b523b1052a06d8a046764
Michael Hucka <mhucka@caltech.edu>
2017-11-29 12:49:41 -0800

    Add version.py

---
+++ cloison/version.py
@@ -0,0 +1,15 @@
+# =============================================================================
+# @file    version.py
+# @brief   CASICS Cloison version info
+# @author  Michael Hucka <mhucka@caltech.edu>
+# @license Please see the file named LICENSE in the project directory
+# @website https://github.com/casics/cloison
+# =============================================================================
+
+__version__   = '1.0.0'
+__title__     = 'Cloison'
+__url__       = 'https://github.com/casics/cloison'
+__author__    = 'Michael Hucka <mhucka@caltech.edu>'
+__email__     = 'casics-team@googlegroups.com'
+__license__   = 'GNU General Public License, version 3.0'
+__copyright__ = 'Copyright (C) 2015-2017 by the California Institute of Technology'
