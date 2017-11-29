a292f998f473792491fa79e522c50240611c03e8
Michael Hucka <mhucka@caltech.edu>
2017-11-29 12:47:49 -0800

    Expose more functions

--- cloison/__init__.py
+++ cloison/__init__.py
@@ -1,2 +1,3 @@
-from .simple_splitters import delimiter_split, safe_camelcase_split
+from .simple_splitters import delimiter_split, digit_split, safe_camelcase_split
+from .simple_splitters import safe_simple_split, simple_split
 from .samurai import same_case_split, samurai_split
