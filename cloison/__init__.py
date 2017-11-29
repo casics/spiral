2606938f692073be52e5e862dc83d631e96e0dbc
Michael Hucka <mhucka@caltech.edu>
2017-11-29 12:50:21 -0800

    Add import of version.py

--- cloison/__init__.py
+++ cloison/__init__.py
@@ -1,3 +1,12 @@
+# =============================================================================
+# @file    __init__.py
+# @brief   CASICS Cloison package __init__ file
+# @author  Michael Hucka <mhucka@caltech.edu>
+# @license Please see the file named LICENSE in the project directory
+# @website https://github.com/casics/cloison
+# =============================================================================
+
+from .version import *
 from .simple_splitters import delimiter_split, digit_split, safe_camelcase_split
 from .simple_splitters import safe_simple_split, simple_split
 from .samurai import same_case_split, samurai_split
