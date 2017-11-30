3c1d4ec16f4a615bbc12753fd0ffaf5590628ffc
Michael Hucka <mhucka@caltech.edu>
2017-11-29 19:54:16 -0800

    Fix up setup.py and requirements.txt
    Former-commit-id: 75ad20e6d047857c66ba5b0a1c1f766575495a56

--- setup.py
+++ setup.py
@@ -11,22 +11,25 @@ import os
 from   setuptools import setup, find_packages
 import sys
 
+import cloison
+
 here = os.path.abspath(os.path.dirname(__file__))
 
 with open(os.path.join(here, 'requirements.txt')) as f:
     reqs = f.read().rstrip().splitlines()
 
 setup(
-    name=nostril.__version__.__title__.lower(),
-    description=nostril.__version__.__description__,
+    name=cloison.__version__.__title__.lower(),
+    description=cloison.__version__.__description__,
     long_description='Cloison (CLeaver Of Identifiers in SOurce code coNtent) provides methods for splitting and expanding identifiers found in source code files.',
     keywords="program-analysis text-processing machine-learning",
-    version=nostril.__version__.__version__,
-    url=nostril.__version__.__url__,
-    author=nostril.__version__.__author__,
-    author_email=nostril.__version__.__email__,
-    license=nostril.__version__.__license__,
+    version=cloison.__version__.__version__,
+    url=cloison.__version__.__url__,
+    author=cloison.__version__.__author__,
+    author_email=cloison.__version__.__email__,
+    license=cloison.__version__.__license__,
     packages=['cloison'],
+    data_files=[('cloison', ['cloison/frequencies.pickle', 'cloison/frequencies.csv'])],
     install_requires=reqs,
     platforms='any',
 )
