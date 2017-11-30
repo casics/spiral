b99da3f3681c46c89d944cfee9ec75f71163878b
Michael Hucka <mhucka@caltech.edu>
2017-11-29 18:14:29 -0800

    Rename version.py to __version__.py

---
+++ cloison/__version__.py
@@ -0,0 +1,16 @@
+# =============================================================================
+# @file    __version__.py
+# @brief   CASICS Cloison version info
+# @author  Michael Hucka <mhucka@caltech.edu>
+# @license Please see the file named LICENSE in the project directory
+# @website https://github.com/casics/cloison
+# =============================================================================
+
+__version__     = '1.0.0'
+__title__       = 'Cloison'
+__description__ = 'CLeaver Of Identifiers in SOurce code coNtent'
+__url__         = 'https://github.com/casics/cloison'
+__author__      = 'Michael Hucka <mhucka@caltech.edu>'
+__email__       = 'casics-team@googlegroups.com'
+__license__     = 'GNU General Public License, version 3.0'
+__copyright__   = 'Copyright (C) 2015-2017 by the California Institute of Technology'
