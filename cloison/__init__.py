acf5b782725a53c60529a65ee148ab1e2ad421d5
Michael Hucka <mhucka@caltech.edu>
2017-11-29 18:14:44 -0800

    Rename version.py to __version__.py

--- cloison/__init__.py
+++ cloison/__init__.py
@@ -6,7 +6,7 @@
 # @website https://github.com/casics/cloison
 # =============================================================================
 
-from .version import *
+from .__version__ import *
 from .simple_splitters import delimiter_split, digit_split, safe_camelcase_split
 from .simple_splitters import safe_simple_split, simple_split
 from .samurai import same_case_split, samurai_split
