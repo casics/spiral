5aa3f9f0e8f94c788521cc8ee4fc66557a7ee24b
Michael Hucka <mhucka@caltech.edu>
2017-11-30 22:12:36 -0800

    Rename Cloison -> Spiral

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # =============================================================================
 # @file    setup.py
-# @brief   Cloison setup file
+# @brief   Spiral setup file
 # @author  Michael Hucka <mhucka@caltech.edu>
 # @license Please see the file named LICENSE in the project directory
 # @website https://github.com/casics/nostril
@@ -11,7 +11,7 @@ import os
 from   setuptools import setup, find_packages
 import sys
 
-import cloison
+import spiral
 
 here = os.path.abspath(os.path.dirname(__file__))
 
@@ -19,17 +19,17 @@ with open(os.path.join(here, 'requirements.txt')) as f:
     reqs = f.read().rstrip().splitlines()
 
 setup(
-    name=cloison.__version__.__title__.lower(),
-    description=cloison.__version__.__description__,
-    long_description='Cloison (CLeaver Of Identifiers in SOurce code coNtent) provides methods for splitting and expanding identifiers found in source code files.',
+    name=spiral.__version__.__title__.lower(),
+    description=spiral.__version__.__description__,
+    long_description='Spiral (SPlitters for IdentifieRs: A Library) provides methods for splitting and expanding identifiers found in source code files.',
     keywords="program-analysis text-processing machine-learning",
-    version=cloison.__version__.__version__,
-    url=cloison.__version__.__url__,
-    author=cloison.__version__.__author__,
-    author_email=cloison.__version__.__email__,
-    license=cloison.__version__.__license__,
-    packages=['cloison'],
-    data_files=[('cloison', ['cloison/frequencies.pickle', 'cloison/frequencies.csv'])],
+    version=spiral.__version__.__version__,
+    url=spiral.__version__.__url__,
+    author=spiral.__version__.__author__,
+    author_email=spiral.__version__.__email__,
+    license=spiral.__version__.__license__,
+    packages=['spiral'],
+    data_files=[('spiral', ['spiral/frequencies.pickle', 'spiral/frequencies.csv'])],
     install_requires=reqs,
     platforms='any',
 )
