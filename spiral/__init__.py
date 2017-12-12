f6590dbe205ee0576bf4e3404cf6d3ce6839818a
Michael Hucka <mhucka@caltech.edu>
2017-12-12 11:41:28 -0800

    New name for modified Samurai algorithm is "ronin"

--- spiral/__init__.py
+++ spiral/__init__.py
@@ -7,6 +7,13 @@
 # =============================================================================
 
 from .__version__ import *
+
+# Supporting modules.
+from .frequencies import init_word_frequencies
+
+# Simple splitters.
 from .simple_splitters import delimiter_split, digit_split, safe_camelcase_split
 from .simple_splitters import safe_simple_split, simple_split
-from .samurai import same_case_split, samurai_split
+
+# Advanced splitters.
+from .ronin import ronin_split
