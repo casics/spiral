53ca49a316bb7a0ce0e69df1a04d9ff242023fe2
Michael Hucka <mhucka@caltech.edu>
2017-12-12 11:45:23 -0800

    Rename test functions for more clarity & add more tests

--- tests/test_simple_splitters.py
+++ tests/test_simple_splitters.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
 
+import os
 import pytest
 import sys
-import glob
+from   time import time
 
-sys.path.append('../')
-sys.path.append('../../common')
-sys.path.append('../../extractor')
+thisdir = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(thisdir, '..'))
 
-from simple_splitters import *
+from spiral import *
 
 class TestClass:
-    def test_camel_split(self, capsys):
+    def test_safe_camelcase_split(self, capsys):
         assert safe_camelcase_split('lower')          == ['lower']
         assert safe_camelcase_split('fooBar')         == ['foo', 'Bar']
         assert safe_camelcase_split('FooBar')         == ['Foo', 'Bar']
@@ -23,7 +23,8 @@ class TestClass:
         assert safe_camelcase_split('SqlList')        == ['Sql', 'List']
         assert safe_camelcase_split('jLabel')         == ['j', 'Label']
 
-    def test_simple_split(self, capsys):
+
+    def test_safe_simple_split(self, capsys):
         assert safe_simple_split('fooBar2day')     == ['foo', 'Bar', 'day']
         assert safe_simple_split('foo_bar')        == ['foo', 'bar']
         assert safe_simple_split('foo_bar2day')    == ['foo', 'bar', 'day']
@@ -40,3 +41,11 @@ class TestClass:
         assert safe_simple_split('ASTVisitor')     == ['ASTVisitor']
         assert safe_simple_split('SqlList')        == ['Sql', 'List']
         assert safe_simple_split('jLabel')         == ['j', 'Label']
+        assert safe_simple_split('aFastNDecoder')  == ['aFastNDecoder']
+
+
+    def test_simple_split(self, capsys):
+        assert simple_split('fooBar2day')    == ['foo', 'Bar', 'day']
+        assert simple_split('SQLlite')       == ['SQLlite']
+        assert simple_split('aFastNDecoder') == ['a', 'Fast', 'NDecoder']
+        assert simple_split('getMAX')        == ['get', 'MAX']
