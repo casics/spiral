f8c80335b565e688bc522e3d589ee54fcc7e380d
Michael Hucka <mhucka@caltech.edu>
2018-01-09 15:33:59 -0800

    1) Add new heuristic_split(); 2) tweak existing code a bit

--- spiral/simple_splitters.py
+++ spiral/simple_splitters.py
@@ -7,6 +7,7 @@ None of these make inferences or expand identifiers.
 '''
 
 import re
+import string
 import sys
 
 try:
@@ -15,12 +16,14 @@ except:
     sys.path.append("..")
 
 try:
+    from .constants import *
     from .utils import flatten
 except:
+    from constants import *
     from utils import flatten
 
 
-# Delimiter-based splitter
+# Delimiter-based splitter.
 # .............................................................................
 #
 # This does nothing fancy. It splits by explicit delimiter characters like '_'.
@@ -35,8 +38,7 @@ def delimiter_split(identifier):
     return parts
 
 
-_digit_chars = '0123456789'
-_digit_splitter = str.maketrans(_digit_chars, ' '*len(_digit_chars))
+_digit_splitter = str.maketrans(string.digits, ' '*len(string.digits))
 
 def digit_split(identifier):
     '''Split identifier at digits only.'''
@@ -45,7 +47,7 @@ def digit_split(identifier):
     return parts
 
 
-# Safe camel case splitter
+# Safe camel case splitter.
 # .............................................................................
 
 _two_capitals = re.compile(r'[A-Z][A-Z]')
@@ -61,30 +63,30 @@ def safe_camelcase_split(identifier):
     return re.sub(_camel_case, r' \1', identifier).split()
 
 
-# Safe simple splitter
+# Safe simple splitter.
 # .............................................................................
 
-_hard_split_chars = '~_.:0123456789'
+_hard_split_chars = '$~_.:/'
 _hard_splitter = str.maketrans(_hard_split_chars, ' '*len(_hard_split_chars))
 
 def safe_simple_split(identifier):
-    '''Split identifiers by hard delimiters such as underscores, digits, and
+    '''Split identifiers by hard delimiters ('_', '$', '~', '.', ':') and
     forward camel case only, i.e., lower-to-upper case transitions.  This
-    means it will split fooBarBaz into 'foo', 'Bar' and 'Baz', and foo2bar
-    into 'foo' and 'bar, but it won't change SQLlite or similar identifiers.
-    Does not split identifies that have multiple adjacent uppercase
-    letters anywhere in them, because doing so is risky if the uppercase
-    letters are not an acronym.  Example: aFastNDecoder -> ['aFastNDecoder'].
-    Contrast this to simple_split('aFastNDecoder'), which will produce
-    ['a', 'Fast', 'NDecoder'] even though "NDecoder" may be more properly split
-    as 'N' 'Decoder'.
+    means it will split fooBarBaz into 'foo', 'Bar' and 'Baz', but it won't
+    split SQLlite or similar identifiers.  Does not split identifies that
+    have multiple adjacent uppercase letters anywhere in them, because doing
+    so is risky if the uppercase letters are not an acronym.  Example:
+    aFastNDecoder -> ['aFastNDecoder'].  Digits are not removed from strings.
     '''
-    parts = str.translate(identifier, _hard_splitter).split(' ')
+    parts = str.translate(identifier, _hard_splitter).split()
     return list(flatten(safe_camelcase_split(token) for token in parts))
 
 
-# Not-so-safe simple splitter
+# Not-so-safe simple splitter.
 # .............................................................................
+
+_simple_split_chars = '$~_.:/0123456789'
+_simple_splitter = str.maketrans(_simple_split_chars, ' '*len(_simple_split_chars))
 
 def simple_split(identifier):
     '''Split identifiers by hard delimiters such as underscores, digits, and
@@ -93,7 +95,52 @@ def simple_split(identifier):
     into 'foo' and 'bar, but it won't change SQLlite or similar identifiers.
     Unlike safe_simple_split(), this will split identifiers that may have
     sequences of all upper-case letters if there is a lower-to-upper case
-    transition somewhere.  Example: ABCtestSplit -> ['ABCtest', 'Split'].
+    transition somewhere.  Example: simple_split('aFastNDecoder') will
+    produce ['a', 'Fast', 'NDecoder'] even though "NDecoder" may be more
+    correctly split as 'N' 'Decoder'. Digits are removed from the string.
     '''
-    parts = str.translate(identifier, _hard_splitter)
+    parts = str.translate(identifier, _simple_splitter)
     return re.sub(_camel_case, r' \1', parts).split()
+
+
+# Not-so-safe, not-so-simple splitter.
+# .............................................................................
+
+def heuristic_split(identifier):
+    '''Split identifiers by hard delimiters such as underscores, digits, and
+    forward camel case only, i.e., lower-to-upper case transitions.  This
+    means it will split fooBarBaz into 'foo', 'Bar' and 'Baz', and foo2bar
+    into 'foo' and 'bar, but it won't change SQLlite or similar identifiers.
+    Unlike safe_simple_split(), this will split identifiers that may have
+    sequences of all upper-case letters if there is a lower-to-upper case
+    transition somewhere.  Example: heuristic_split('aFastNDecoder') will
+    produce ['a', 'Fast', 'NDecoder'] even though "NDecoder" may be more
+    correctly split as 'N' 'Decoder'.  Leading digits are removed from the
+    identifier string before processing; embedded digits are removed if they
+    appear at the tail ends of tokens, but not if they appear in the middle
+    of tokens or are recognized as being part of common abbreviations such as
+    'utf8'.  Tokens that consist ONLY of digits are removed.
+    '''
+    parts = str.translate(identifier.lstrip(string.digits), _hard_splitter)
+    return _filter_digit_strings(re.sub(_camel_case, r' \1', parts).split())
+
+
+# Internal utilities.
+# .............................................................................
+
+_only_digits = re.compile(r'\d')
+_non_digits  = re.compile(r'\D')
+_recognized_suffixes = re.compile(r'\D' + '|'.join(common_suffix_numbers))
+
+def _filter_digit_strings(lst):
+    results = []
+    for token in lst:
+        if token.lower() in common_terms_with_numbers:
+            results.append(token)
+        elif re.search(_recognized_suffixes, token):
+            results.append(token)
+        elif re.search(_non_digits, token):
+            results.append(token.rstrip(string.digits))
+        elif not re.search(_only_digits, token):
+            results.append(token)
+    return results
