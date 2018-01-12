b3bb1eb3a0933ef70b74fe789712c023260e4848
Michael Hucka <mhucka@caltech.edu>
2018-01-11 21:06:06 -0800

    Add utility to print differences in simple splitters
    Former-commit-id: 66c8bc58b4711845f563c7a845d9dbc326d45252

---
+++ tests/show-simple.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# =============================================================================
+# @file    show-simple.py
+# @brief   Show differences in behaviors of the simple splitters
+# @author  Michael Hucka <mhucka@caltech.edu>
+# @license Please see the file named LICENSE in the project directory
+# @website https://github.com/casics/spiral
+# =============================================================================
+
+import os
+import sys
+
+try:
+    sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+except:
+    sys.path.append("..")
+
+from spiral.simple_splitters import pure_camelcase_split, safe_simple_split
+from spiral.simple_splitters import simple_split, elementary_split
+from spiral.simple_splitters import heuristic_split
+
+cases = [
+    'alllower',
+    'ALLUPPER',
+    'a_delimiter',
+    'a.delimiter',
+    'a$delimiter',
+    'a:delimiter',
+    'a_fooBar',
+    'fooBar',
+    'FooBar',
+    'Foobar',
+    'fooBAR',
+    'fooBARbif',
+    'fooBARbifBaz',
+    'ABCfoo',
+    'ABCFoo',
+    'ABCFooBar',
+    'ABCfooBar',
+    'fooBar2day',
+    'fooBar2Day',
+    'fooBAR2day',
+    'fooBAR2Day',
+    'Foo2Bar',
+    '2foo2bar',
+    '2Foo2bar',
+    '2Foo2Bar',
+    '2foo2bar2',
+    'The2ndVar',
+]
+
+def use(splitter, s):
+    return ' '.join(splitter(s)).ljust(16)
+
+print('input'.ljust(16) + 'pure_camel'.ljust(16) + 'safe_simple'.ljust(16)
+      + 'simple'.ljust(16) + 'elementary'.ljust(16) + 'heuristic'.ljust(16))
+print('-------'.ljust(16) + '-------'.ljust(16) + '-------'.ljust(16)
+      + '-------'.ljust(16) + '-------'.ljust(16) + '-------'.ljust(16))
+for s in cases:
+    print(s.ljust(16)
+          + use(pure_camelcase_split, s)
+          + use(safe_simple_split, s)
+          + use(simple_split, s)
+          + use(elementary_split, s)
+          + use(heuristic_split, s))
