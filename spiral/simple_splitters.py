98fdb92e18be315c3662ffae272eb2aefde14ed1
Michael Hucka <mhucka@caltech.edu>
2018-01-11 21:46:19 -0800

    Improve heuristic_split and add table comparing splitters
    1. Realized I could use the common terms as splitting boundaries by
    searching for them in the input string before doing camel-case
    splitting.  Implemented this in heuristic_split(), which now performs
    even better.
    
    2. Added a table to the top of the file showing how the different
    splitters behave for different input strings.
    
    
    Former-commit-id: 5aa913f904acdaef8abd6304be53c3f971c01ab0

--- spiral/simple_splitters.py
+++ spiral/simple_splitters.py
@@ -1,9 +1,48 @@
-'''
-simple_splitters: simple identifier splitters
+'''simple_splitters: simple identifier splitters
 
-This exports a number of simple splitter functions whose behaviors are sightly
-different depending on what assumptions are made about identifier patterns.
-None of these make inferences or expand identifiers.
+This exports a number of simple splitter functions whose behaviors are
+sightly different depending on what assumptions are made about identifier
+patterns.  All are simple in the sense that they do not make complicated
+inferences about the string and at most (in the cause of heuristic_split) use
+fixed dictionaries of predefined terms to enhance performance.
+
+The following table illustrates the differences between the different splitters
+implemented in this file.  The first column shows the input string; the
+remaining columns show the results of the different X_split() functions.
+
+input          pure_camel     safe_simple    simple          elementary      heuristic
+-------        -------        -------        -------         -------         -------
+alllower       alllower       alllower       alllower        alllower        alllower
+ALLUPPER       ALLUPPER       ALLUPPER       ALLUPPER        ALLUPPER        ALLUPPER
+a_delimiter    a_delimiter    a delimiter    a delimiter     a delimiter     a delimiter
+a.delimiter    a.delimiter    a delimiter    a delimiter     a delimiter     a delimiter
+a$delimiter    a$delimiter    a delimiter    a delimiter     a delimiter     a delimiter
+a:delimiter    a:delimiter    a delimiter    a delimiter     a delimiter     a delimiter
+a_fooBar       a_foo Bar      a foo Bar      a foo Bar       a foo Bar       a foo Bar
+fooBar         foo Bar        foo Bar        foo Bar         foo Bar         foo Bar
+FooBar         Foo Bar        Foo Bar        Foo Bar         Foo Bar         Foo Bar
+Foobar         Foobar         Foobar         Foobar          Foobar          Foobar
+fooBAR         fooBAR         fooBAR         foo BAR         foo BAR         foo BAR
+fooBARbif      fooBARbif      fooBARbif      foo BARbif      foo BARbif      foo BARbif
+fooBARbifBaz   fooBARbifBaz   fooBARbifBaz   foo BARbif Baz  foo BARbif Baz  foo BARbif Baz
+ABCfoo         ABCfoo         ABCfoo         ABCfoo          ABCfoo          ABCfoo
+ABCFoo         ABCFoo         ABCFoo         ABCFoo          ABCFoo          ABCFoo
+ABCFooBar      ABCFooBar      ABCFooBar      ABCFoo Bar      ABCFoo Bar      ABCFoo Bar
+ABCfooBar      ABCfooBar      ABCfooBar      ABCfoo Bar      ABCfoo Bar      ABCfoo Bar
+fooBar2day     foo Bar2day    foo Bar2day    foo Bar2day     foo Bar 2 day   foo Bar2day
+fooBar2Day     foo Bar2Day    foo Bar2Day    foo Bar2Day     foo Bar 2 Day   foo Bar2Day
+fooBAR2day     fooBAR2day     fooBAR2day     foo BAR2day     foo BAR 2 day   foo BAR2day
+fooBAR2Day     fooBAR2Day     fooBAR2Day     foo BAR2Day     foo BAR 2 Day   foo BAR2Day
+Foo2Bar        Foo2Bar        Foo2Bar        Foo2Bar         Foo 2 Bar       Foo2Bar
+2foo2bar       2foo2bar       2foo2bar       2foo2bar        2 foo 2 bar     foo2bar
+2Foo2bar       2Foo2bar       2Foo2bar       2Foo2bar        2 Foo 2 bar     Foo2bar
+2Foo2Bar       2Foo2Bar       2Foo2Bar       2Foo2Bar        2 Foo 2 Bar     Foo2Bar
+2foo2bar2      2foo2bar2      2foo2bar2      2foo2bar2       2 foo 2 bar 2   foo2bar
+The2ndVar      The2nd Var     The2nd Var     The2nd Var      The 2 nd Var    The2nd Var
+row10          row10          row10          row10           row 10          row
+utf8           utf8           utf8           utf8            utf 8           utf8
+aUTF8var       aUTF8var       aUTF8var       a UTF8var       a UTF 8 var     a UTF8 var
+J2SE4me        J2SE4me        J2SE4me        J2SE4me         J 2 SE 4 me     J2SE 4me
 '''
 
 import re
@@ -129,6 +168,8 @@ def elementary_split(identifier):
 # Not-so-safe, not-so-simple splitter.
 # .............................................................................
 
+_common_terms = re.compile(r'(' + '|'.join(common_terms_with_numbers) + ')', re.IGNORECASE)
+
 def heuristic_split(identifier):
     '''Split identifiers by hard delimiters such as underscores, digits, and
     forward camel case only, i.e., lower-to-upper case transitions.  This
@@ -145,6 +186,7 @@ def heuristic_split(identifier):
     'utf8'.  Tokens that consist ONLY of digits are removed.
     '''
     parts = str.translate(identifier.lstrip(string.digits), _hard_splitter)
+    parts = re.sub(_common_terms, r' \1 ', parts)
     return _filter_digit_strings(re.sub(_camel_case, r' \1', parts).split())
 
 
