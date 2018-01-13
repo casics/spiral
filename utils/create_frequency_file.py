4a44a2784ee1d12f7f2b5f8273bd241df42e99ed
Michael Hucka <mhucka@caltech.edu>
2018-01-12 18:10:02 -0800

    New version that now generates a frequency pickle or csv file directly

--- utils/create_frequency_file.py
+++ utils/create_frequency_file.py
@@ -1,14 +1,11 @@
-#!/usr/bin/env python3.4
-#
+#!/usr/bin/env python3
+# =============================================================================
 # @file    create_frequency_file.py
-# @brief   Encapsulates the steps to create frequency.csv from raw word frequencies
+# @brief   Create a frequency.csv or frequency.pklz from raw token frequencies
 # @author  Michael Hucka
-#
-# <!---------------------------------------------------------------------------
-# Copyright (C) 2015 by the California Institute of Technology.
-# This software is part of CASICS, the Comprehensive and Automated Software
-# Inventory Creation System.  For more information, visit http://casics.org.
-# ------------------------------------------------------------------------- -->
+# @license Please see the file named LICENSE in the project directory
+# @website https://github.com/casics/extractor
+# =============================================================================
 
 import csv
 import math
@@ -16,78 +13,60 @@ import plac
 import re
 import sys
 
-sys.path.append('../common')
+try:
+    sys.path.append(os.path.join(os.path.dirname(__file__), "../spiral"))
+except:
+    sys.path.append("../spiral")
 
-from utils import *
-from logger import *
-
-
-# Utilities
-# .............................................................................
-
-def filter_simple(token, frequency):
-    '''Single letters, or repeated 2-letter sequences such as 'aa' or 'Bb'.'''
-    return len(w) == 1 or (len(w) == 2 and w[0].lower() == w[1].lower())
-
-
-def filter_special(token, frequency):
-    '''Things that end up in the token lists but are not useful.'''
-    return 'py' == token.lower()
-
-
-def filter_by_frequency(token, frequency):
-    return int(frequency) < 30
-
-
-# 2017-02-15 <mhucka@caltech.edu>
-# Further testing and tweaking of the code in samurai.py ended up making this
-# filter of the frequency data undesirable.  Leaving all this code in place
-# in case we change our mind in the future, but for now, this is set to
-# never filter anything.
-def undesirable(token, frequency):
-    return False
-    # return (filter_simple(token, frequency)
-    #         or filter_special(token, frequency)
-    #         or filter_by_frequency(token, frequency))
+import frequencies
 
 
 # Main
 # .............................................................................
 
-def create_frequency_file(inputfile=None, outputfile=None, debug=False,
-                          loglevel='info'):
-    log = Logger(os.path.splitext(sys.argv[0])[0], console=True).get_log()
-    if debug:
-        log.set_level('debug')
-    else:
-        log.set_level(loglevel)
-    try:
-        with open(inputfile, 'r') as input:
-            with open(outputfile, 'w') as output:
-                if debug:
-                    import ipdb; ipdb.set_trace()
-                total = 0
-                for line in input:
-                    (token, frequency) = line.split()
-                    if undesirable(token, frequency):
-                        log.debug('skipping undesirable token {}'.format(token))
-                    else:
-                        output.write(token)
-                        output.write(',')
-                        output.write(frequency)
-                        output.write('\n')
-                        total += 1
-                log.info('{} tokens written to {}'.format(total, outputfile))
-    except Exception as err:
-        log.error(err)
-
-
-create_frequency_file.__annotations__ = dict(
-    debug      = ('drop into ipdb opening files', 'flag',   'd'),
-    inputfile  = ('input text file',              'option', 'i'),
-    outputfile = ('output CSV file',              'option', 'o'),
-    loglevel   = ('logging level: "debug" or "info"', 'option', 'L'),
+@plac.annotations(
+    debug      = ('drop into ipdb opening files',        'flag',   'd'),
+    inputfile  = ('input text file',                     'option', 'i'),
+    outputfile = ('output file',                         'option', 'o'),
 )
 
+def main(inputfile=None, outputfile=None, debug=False):
+    '''The intput file should be a plain-text table of frequencies, with each
+line consisting of a token, some whitespace, and an integer.  The output
+format is based on the file extension: .csv for a CSV file, .pklz for a
+compressed pickle file.
+'''
+    if not inputfile:
+        raise SystemExit('Missing input file argument.')
+    if not outputfile:
+        raise SystemExit('Missing output file argument.')
+    try:
+        data = {}
+        with open(inputfile, 'r') as input:
+            if debug:
+                import ipdb; ipdb.set_trace()
+            total = 0
+            for line in input:
+                (token, frequency) = line.split()
+                data[token] = int(frequency)
+                total += 1
+        print('{} read lines from {}'.format(total, inputfile))
+        if outputfile.endswith('.csv'):
+            with open(outputfile, 'w') as output:
+                for token, frequency in sorted(data.items(), reverse=True,
+                                               key=lambda x: x[1]):
+                    output.write(token)
+                    output.write(',')
+                    output.write(str(frequency))
+                    output.write('\n')
+        else:
+            frequencies.save_frequencies_to_pickle(data, outputfile)
+    except Exception as err:
+        print(err)
+
+
+# Entry point.
+# .............................................................................
+
 if __name__ == '__main__':
-    plac.call(create_frequency_file)
+    plac.call(main)
