f528c9762f6f1398c2c489c8037cfc9e88930b70
Michael Hucka <mhucka@caltech.edu>
2018-01-14 10:27:47 -0800

    Add support for thresholding in frequencies_from_{csv,pickle}
    Former-commit-id: 4ee62be9c4d5099712580350a6a56abffa22a0c9

--- spiral/frequencies.py
+++ spiral/frequencies.py
@@ -6,35 +6,57 @@ frequencies: code to handle word frequencies
 # Main code.
 # .............................................................................
 
-def frequencies_from_csv_file(filename, filter_words=None):
+def frequencies_from_csv_file(filename, threshold=None, filter_words=None):
+    '''Read a table of frequencies (as a Python dictionary) from a CSV file.
+    Parameter 'threshold' sets a minimum frequency value for entries to keep.
+    Entries with frequency values less than the given 'threshold' value will
+    be discarded.  Parameter 'filter_word' is a set of words (dictionary keys)
+    that should be ignored.  Entries that are found in 'filter_words' will be
+    discarded.  Returns a dictionary of the results.
+    '''
     import csv
     try:
         with open(filename, 'r') as f:
             reader = csv.DictReader(f, fieldnames=['word','frequency'])
             frequencies = {}
             for row in reader:
-                if filter_words and row['word'] in filter_words:
+                word = row['word']
+                frequency = int(row['frequency'])
+                if threshold and frequency < threshold:
                     continue
-                frequencies[row['word']] = int(row['frequency'])
+                if filter_words and word in filter_words:
+                    continue
+                frequencies[word] = frequency
             return frequencies
     except Exception as err:
         return {}
 
 
-def frequencies_from_pickle(filename, filter_words=None):
-    '''Read a pickle file.  If the file ends in the extension .pklz, it's
-    assumed to be a gzip'ed pickle file; otherwise, it's assumed to be an
-    uncompressed pickle file.
+def frequencies_from_pickle(filename, threshold=None, filter_words=None):
+    '''Read a table of frequencies (as a Python dictionary) from a pickle file.
+    If the file name ends in the extension .pklz, it's assumed to be a gzip'ed
+    pickle file; otherwise, it's assumed to be an uncompressed pickle file.
+    Parameter 'threshold' sets a minimum frequency value for entries to keep.
+    Entries with frequency values less than the given 'threshold' value will
+    be discarded.  Parameter 'filter_word' is a set of words (dictionary keys)
+    that should be ignored.  Entries that are found in 'filter_words' will be
+    discarded.  Returns a dictionary of the results.
     '''
     import pickle
+    data = {}
     try:
         if filename.endswith('.pklz'):
             import gzip
             with gzip.open(filename, 'rb') as pickle_file:
-                return pickle.load(pickle_file)
+                data = pickle.load(pickle_file)
         else:
             with open(filename, 'rb') as pickle_file:
-                return pickle.load(pickle_file)
+                data = pickle.load(pickle_file)
+        if threshold:
+            data = {k : v for k, v in data if v < threshold}
+        if filter_words:
+            data = {k : v for k, v in data if k not in filter_words}
+        return data
     except Exception as err:
         return {}
 
