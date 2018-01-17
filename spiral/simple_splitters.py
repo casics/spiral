0ee77031a683a0a1b5ffa6c9b124c7516939264c
Michael Hucka <mhucka@caltech.edu>
2018-01-17 10:14:11 -0800

    Change heuristic_split to keep digits
    Changes elsewhere in Spiral made me realize it's more sensible to keep
    numbers by default, and offer a flag to heuristic_split to remove the
    numbers on demand only.
    
    Other changes are for documentation or refactoring or tweaking.
    
    
    Former-commit-id: 9b576834c9b2691d1237b78347a72e88038e6f02

--- spiral/simple_splitters.py
+++ spiral/simple_splitters.py
@@ -29,22 +29,25 @@ ABCfoo       ABCfoo        ABCfoo       ABCfoo        ABCfoo         ABCfoo
 ABCFoo       ABCFoo        ABCFoo       ABCFoo        ABCFoo         ABCFoo
 ABCFooBar    ABCFoo Bar    ABCFooBar    ABCFoo Bar    ABCFoo Bar     ABCFoo Bar
 ABCfooBar    ABCfoo Bar    ABCfooBar    ABCfoo Bar    ABCfoo Bar     ABCfoo Bar
-fooBar2day   foo Bar2day   foo Bar2day  foo Bar2day   foo Bar 2 day  foo Bar2day
-fooBar2Day   foo Bar2Day   foo Bar2Day  foo Bar2Day   foo Bar 2 Day  foo Bar2Day
-fooBAR2day   foo BAR2day   fooBAR2day   foo BAR2day   foo BAR 2 day  foo BAR2day
-fooBAR2Day   foo BAR2Day   fooBAR2Day   foo BAR2Day   foo BAR 2 Day  foo BAR2Day
-foo3000      foo3000       foo3000      foo3000       foo 3000       foo
-99foo3000    99foo3000     99foo3000    99foo3000     99 foo 3000    foo
-foo2Bar      foo2Bar       foo2Bar      foo2Bar       foo 2 Bar      foo2Bar
-foo2bar2     foo2bar2      foo2bar2     foo2bar2      foo 2 bar 2    foo2bar
-Foo2Bar2     Foo2Bar2      Foo2Bar2     Foo2Bar2      Foo 2 Bar 2    Foo2Bar
+fooBar2day   foo Bar2day   foo Bar2day  foo Bar2day   foo Bar 2 day  foo Bar 2 day
+fooBar2Day   foo Bar2Day   foo Bar2Day  foo Bar2Day   foo Bar 2 Day  foo Bar 2 Day
+fooBAR2day   foo BAR2day   fooBAR2day   foo BAR2day   foo BAR 2 day  foo BAR 2 day
+fooBAR2Day   foo BAR2Day   fooBAR2Day   foo BAR2Day   foo BAR 2 Day  foo BAR 2 Day
+foo3000      foo3000       foo3000      foo3000       foo 3000       foo 3000
+99foo3000    99foo3000     99foo3000    99foo3000     99 foo 3000    99 foo 3000
+foo2Bar      foo2Bar       foo2Bar      foo2Bar       foo 2 Bar      foo 2 Bar
+foo2bar2     foo2bar2      foo2bar2     foo2bar2      foo 2 bar 2    foo 2 bar 2
+Foo2Bar2     Foo2Bar2      Foo2Bar2     Foo2Bar2      Foo 2 Bar 2    Foo 2 Bar 2
+MyInt32      My Int32      My Int32     My Int32      My Int 32      My Int32
+MyInt42      My Int42      My Int42     My Int42      My Int 42      My Int 42
+MyInt32Var   My Int32Var   My Int32Var  My Int32Var   My Int 32 Var  My Int32 Var
 2ndvar       2ndvar        2ndvar       2ndvar        2 ndvar        2nd var
 the2ndvar    the2ndvar     the2ndvar    the2ndvar     the 2 ndvar    the 2nd var
 the2ndVar    the2nd Var    the2nd Var   the2nd Var    the 2 nd Var   the 2nd Var
-row10        row10         row10        row10         row 10         row
+row10        row10         row10        row10         row 10         row 10
 utf8         utf8          utf8         utf8          utf 8          utf8
 aUTF8var     a UTF8var     aUTF8var     a UTF8var     a UTF 8 var    a UTF8 var
-J2SE4me      J2SE4me       J2SE4me      J2SE4me       J 2 SE 4 me    J2SE me
+J2SE4me      J2SE4me       J2SE4me      J2SE4me       J 2 SE 4 me    J2SE 4 me
 IPv4addr     IPv4addr      IPv4addr     IPv4addr      IPv 4 addr     IPv4 addr
 '''
 
@@ -54,8 +57,10 @@ import sys
 
 try:
     sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+    sys.path.append(os.path.join(os.path.dirname(__file__), "../spiral"))
 except:
     sys.path.append("..")
+    sys.path.append("../spiral")
 
 try:
     from .constants import *
@@ -65,12 +70,19 @@ except:
     from utils import flatten
 
 
+# Global definitions.
+# .............................................................................
+
+_delimiter_chars = '$~_.:/'
+_two_capitals    = re.compile(r'[A-Z][A-Z]')
+_camel_case      = re.compile(r'((?<=[a-z])[A-Z])')
+
+
 # Delimiter-based splitter.
 # .............................................................................
 #
 # This does nothing fancy. It splits by explicit delimiter characters like '_'.
 
-_delimiter_chars = '_.:'
 _delimiter_splitter = str.maketrans(_delimiter_chars, ' '*len(_delimiter_chars))
 
 def delimiter_split(identifier):
@@ -92,9 +104,6 @@ def digit_split(identifier):
 # Pure camel case splitter.
 # .............................................................................
 
-_two_capitals = re.compile(r'[A-Z][A-Z]')
-_camel_case   = re.compile(r'((?<=[a-z])[A-Z])')
-
 def pure_camelcase_split(identifier, safe=False):
     '''Split identifiers by forward camel case only, i.e., lower-to-upper
     case transitions.  This means it will split fooBarBaz into 'foo', 'Bar'
@@ -114,8 +123,7 @@ def pure_camelcase_split(identifier, safe=False):
 # Safe simple splitter.
 # .............................................................................
 
-_hard_split_chars = '$~_.:/'
-_hard_splitter = str.maketrans(_hard_split_chars, ' '*len(_hard_split_chars))
+_hard_splitter = str.maketrans(_delimiter_chars, ' '*len(_delimiter_chars))
 
 def safe_simple_split(identifier):
     '''Split identifiers by hard delimiters ('_', '$', '~', '.', ':') and
@@ -175,10 +183,10 @@ def elementary_split(identifier):
 # Not-so-safe, not-so-simple splitter.
 # .............................................................................
 
-_common_terms = re.compile(r'(' + '|'.join(common_terms_with_numbers) + ')', re.IGNORECASE)
-_common_suffixes = re.compile(r'\D' + '|'.join(common_suffix_numbers))
+_exceptions_re = re.compile(r'(' + '|'.join(common_terms_with_numbers) + ')', re.I)
+_common_suffixes = tuple(common_suffix_numbers)
 
-def heuristic_split(identifier, exceptions=_common_terms):
+def heuristic_split(identifier, keep_numbers=True, exceptions=None):
     '''Split identifiers by hard delimiters such as underscores, digits, and
     forward camel case only, i.e., lower-to-upper case transitions.  This
     means it will split fooBarBaz into 'foo', 'Bar' and 'Baz', and foo2bar
@@ -187,47 +195,39 @@ def heuristic_split(identifier, exceptions=_common_terms):
     sequences of all upper-case letters if there is a lower-to-upper case
     transition somewhere.  Example: heuristic_split('aFastNDecoder') will
     produce ['a', 'Fast', 'NDecoder'] even though "NDecoder" may be more
-    correctly split as 'N' 'Decoder'.  Leading digits are removed from the
-    identifier string before processing; embedded digits are removed if they
-    appear at the tail ends of tokens, but not if they appear in the middle
-    of tokens or are recognized as being exceptions defined by the compiled
-    regular expression in parameter 'exceptions'.  (The default set of
+    correctly split as 'N' 'Decoder'.  If keep_numbers=False, leading digits
+    are removed from the identifier string before processing; embedded digits
+    are removed if they appear at the tail ends of tokens, but not if they
+    appear in the middle of tokens or are recognized as being exceptions
+    defined by the set in parameter 'exceptions'.  (The default set of
     exceptions are common terms with embedded numbers such as 'utf8'.)
     Tokens that consist ONLY of digits are removed.
     '''
-    split_str = str.translate(identifier, _hard_splitter)
-    split_str = re.sub(exceptions, r' \1 ', split_str)
-    parts = []
-    for s in re.sub(_camel_case, r' \1', split_str).split():
-        if re.search(exceptions, s.lower()) or re.search(_common_suffixes, s):
-            parts.append(s)
-        else:
-            parts.append(re.sub(r'(\d+)', r' \1 ', s).split())
-    return _filter_digit_strings(flatten(parts), exceptions)
+    if exceptions:
+        exceptions_re = re.compile(r'(' + '|'.join(exceptions) + ')', re.I)
+    else:
+        exceptions = common_terms_with_numbers
+        exceptions_re = _exceptions_re
 
-
-# Internal utilities.
-# .............................................................................
-
-_only_digits = re.compile(r'\d')
-_non_digits  = re.compile(r'\D')
-
-def _filter_digit_strings(lst, exceptions):
-    results = []
-    for token in lst:
-        # Test for exceptions before doing anything else.
-        if re.search(exceptions, token.lower()):
-            results.append(token)
-            continue
-        # Strip any leading digits, but only after testing for exceptions.
-        if len(token) >= 1 and token[0].isdigit():
-            token = token.lstrip(string.digits)
-        if len(token) == 0:
-            continue
-        if re.search(_common_suffixes, token):
-            results.append(token)
-        elif re.search(_non_digits, token):
-            results.append(token.rstrip(string.digits))
-        elif not re.search(_only_digits, token):
-            results.append(token)
-    return results
+    preliminary = str.translate(identifier, _hard_splitter)
+    preliminary = re.sub(exceptions_re, r' \1 ', preliminary)
+    preliminary = re.sub(_camel_case, r' \1', preliminary)
+    if keep_numbers:
+        # Unfortunately gnarly and hard-to-read expression.  Did this for the
+        # sake of speed.  Basic idea: keep each token that is in the exceptions
+        # list or ends with a common suffix; otherwise, split the token by digits
+        # and clean up the resulting digit strings.
+        parts = flatten(s if (s.lower() in exceptions
+                              or (not s[0].isdigit() and s.endswith(_common_suffixes)))
+                        else filter(bool, re.split(r'(\d+)', s))
+                        for s in preliminary.split())
+    else:
+        # Variation of the previous thing that will remove leading and trailing
+        # digits of tokens, possibly removing the tokens altogether if the only
+        # characters it has are digits.
+        parts = flatten(s.strip() if (s.lower() in exceptions
+                                      or (not s[0].isdigit() and s.endswith(_common_suffixes)))
+                        else filter(bool, [t.lstrip(string.digits).rstrip(string.digits)
+                                           for t in re.split(r'\d+', s)])
+                        for s in preliminary.split())
+    return list(parts)
