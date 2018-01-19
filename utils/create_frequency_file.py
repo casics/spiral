209f2b0ee1ea69825f089429504f7740d0573e9f
Michael Hucka <mhucka@caltech.edu>
2018-01-19 12:31:02 -0800

    Filter out numbers and some really obvious junk
    Former-commit-id: d0d9e2428f9b4c0ad6c7e5b684f0abf979d2084c

--- utils/create_frequency_file.py
+++ utils/create_frequency_file.py
@@ -32,25 +32,34 @@ import frequencies
 
 def main(inputfile=None, outputfile=None, debug=False):
     '''The intput file should be a plain-text table of frequencies, with each
-line consisting of a token, some whitespace, and an integer.  The output
-format is based on the file extension: .csv for a CSV file, .pklz for a
-compressed pickle file.
+line consisting of a token, some whitespace, and an integer; alternatively,
+the input file can be a csv file in which the first column has the tokens and
+the second column has the frequencies.  The output format is based on the
+file extension: .csv for a CSV file, .pklz for a compressed pickle file.
 '''
     if not inputfile:
         raise SystemExit('Missing input file argument.')
     if not outputfile:
         raise SystemExit('Missing output file argument.')
+    if inputfile.endswith('.csv'):
+        delimiter = ','
+    else:
+        delimiter = (' ', '\t')
     try:
         data = {}
         with open(inputfile, 'r') as input:
             if debug:
                 import ipdb; ipdb.set_trace()
             total = 0
+            kept = 0
             for line in input:
-                (token, frequency) = line.split()
-                data[token] = int(frequency)
                 total += 1
-        print('{} read lines from {}'.format(total, inputfile))
+                (token, frequency) = line.split(delimiter)
+                if filter(token):
+                    continue
+                data[token] = int(frequency)
+                kept += 1
+            print('{} strings read, {} kept.'.format(total, kept))
         if outputfile.endswith('.csv'):
             with open(outputfile, 'w') as output:
                 for token, frequency in sorted(data.items(), reverse=True,
@@ -61,8 +70,34 @@ compressed pickle file.
                     output.write('\n')
         else:
             frequencies.save_frequencies_to_pickle(data, outputfile)
+        print('Output saved in {}.'.format(outputfile))
     except Exception as err:
         print(err)
+
+
+# Note: do not try to filter things that have mixed case and naively might be
+# thought to be multiword identifiers.  I did this originally, thinking it
+# would be safe to filter out strings that start with a capital letter and
+# then have at least one more capital letter followed by a lowercase letter,
+# such as "ABCFoo".  Unfortunately this will catch things like LaTeX and
+# PDFLaTeX, which people do write in mixed case.  This would be bad for our
+# goals.  I can see no safe way of automatically filtering out multiword
+# strings, so this only filters really obviously undesirable stuff.
+
+def filter(s):
+    '''Return True if the token should be filtered out.'''
+    # Filter out pure numbers.
+    # Fast number detector from https://stackoverflow.com/a/23639915/743730
+    if s.replace('.', '', 1).isdigit():
+        return True
+    # Filter out stuff like "e545", "line23", "case2" etc.  Yes, there's a
+    # risk this will catch some acronyms that I'm not aware of, but I think the
+    # risk is low enough that it's okay to do this.  Besides, for Spiral, we
+    # have a separate list of acronyms, and so they will be handled elsewhere.
+    elif re.search('^(e|error|page|line|case|test)[0-9]+$', s, re.IGNORECASE):
+        return True
+    else:
+        return False
 
 
 # Entry point.
