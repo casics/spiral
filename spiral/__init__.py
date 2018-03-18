646f4bad8c7ee75e336fff4696bd998be59b62dc
Michael Hucka <mhucka@caltech.edu>
2018-03-17 21:35:12 -0700

    Add doc string
    Former-commit-id: 18bd49896f5b4ce108b0e9dd5097a3024154c382

--- spiral/__init__.py
+++ spiral/__init__.py
@@ -1,10 +1,33 @@
-# =============================================================================
-# @file    __init__.py
-# @brief   CASICS Spiral package __init__ file
-# @author  Michael Hucka <mhucka@caltech.edu>
-# @license Please see the file named LICENSE in the project directory
-# @website https://github.com/casics/spiral
-# =============================================================================
+'''Spiral: SPlitters for IdentifieRs: A Library
+
+Natural language processing (NLP) methods are increasingly being applied to
+source code analysis for various purposes.  The methods rely on terms
+(identifiers and other textual strings) extracted from program source code
+and comments.  The methods often work better if, instead of raw identifiers,
+real words are used as features; that is, "get" and "integer" are often better
+features for NLP tools than the string "getInteger".  This leads to the
+need for automated methods for splitting identifiers of classes, functions,
+variables, and other entities into word-like constituents.  A number of
+methods have been proposed an implemented to perform identifier splitting.
+
+Spiral is a Python 3 package that implements numerous identifier splitting
+algorithms.  It provides some basic naive splitting algorithms, such as a
+straightforward camel-case splitter, as well as more elaborate heuristic
+splitters, such as an algorithm we call Ronin (originally based on an
+algorithm called Samurai by Enslen, Hill, Pollock and Vija-Shanker [2009]).
+
+Authors
+-------
+
+Michael Hucka <mhucka@caltech.edu>
+
+Copyright
+---------
+
+Copyright (c) 2017 by the California Institute of Technology.  This software
+was developed as part of the CASICS project, the Comprehensive and Automated
+Software Inventory Creation System.  For more, visit http://casics.org.
+'''
 
 from .__version__ import __version__, __title__, __description__, __url__
 from .__version__ import __author__, __email__
