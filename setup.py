fc38d024060be471feccdb4946ad270d21696444
Michael Hucka <mhucka@caltech.edu>
2018-04-29 11:24:28 -0700

    Avoid antipattern of importing spiral from setup.py

--- setup.py
+++ setup.py
@@ -8,31 +8,46 @@
 # =============================================================================
 
 import os
+from   os import path
 from   setuptools import setup, find_packages
 import sys
 
-import spiral
+here = path.abspath(path.dirname(__file__))
 
-here = os.path.abspath(os.path.dirname(__file__))
-
-with open(os.path.join(here, 'requirements.txt')) as f:
+with open(path.join(here, 'requirements.txt')) as f:
     reqs = f.read().rstrip().splitlines()
 
+# The following reads the variables without doing an "import spiral",
+# because the latter will cause the python execution environment to fail if
+# any dependencies are not already installed -- negating most of the reason
+# we're using setup() in the first place.  This code avoids eval, for security.
+
+version = {}
+with open(path.join(here, 'spiral/__version__.py')) as f:
+    text = f.read().rstrip().splitlines()
+    vars = [line for line in text if line.startswith('__') and '=' in line]
+    for v in vars:
+        setting = v.split('=')
+        version[setting[0].strip()] = setting[1].strip().replace("'", '')
+
+# Finally, define our namesake.
+
 setup(
-    name=spiral.__title__.lower(),
-    description=spiral.__description__,
-    long_description='Spiral (SPlitters for IdentifieRs: A Library) provides methods for splitting identifiers found in source code files.',
-    keywords="program-analysis text-processing machine-learning",
-    version=spiral.__version__,
-    url=spiral.__url__,
-    author=spiral.__author__,
-    author_email=spiral.__email__,
-    license=spiral.__license__,
-    packages=['spiral'],
-    package_data={'spiral': ['data/frequencies.pklz',
+    name                 = version['__title__'].lower(),
+    description          = version['__description__'],
+    long_description     = 'Spiral (SPlitters for IdentifieRs: A Library) provides methods for splitting identifiers found in source code files.',
+    version              = version['__version__'],
+    url                  = version['__url__'],
+    author               = version['__author__'],
+    author_email         = version['__email__'],
+    license              = version['__license__'],
+    keywords             = "program-analysis text-processing machine-learning",
+    packages             = ['spiral'],
+    package_data         = {'spiral': ['data/frequencies.pklz',
                              'data/dictionary.pklz']},
-    include_package_data=True,
-    scripts=['bin/spiral'],
-    install_requires=reqs,
-    platforms='any',
+    scripts              = ['bin/spiral'],
+    include_package_data = True,
+    install_requires     = reqs,
+    platforms            = 'any',
+    python_requires  = '>=3',
 )
